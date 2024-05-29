e450145c916d12abdd7946e006a3894d1daf1cf4
Igor Nechaev <igor.nechaev@inbox.ru>
2024-05-29 10:56:36 +0300

    11

---
+++ addressbook-web-test/addressbook-test-data-generators/Удали меня потом.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_test_data_generators
+{
+    public partial class Удали_меня_потом : Component
+    {
+        public Удали_меня_потом()
+        {
+            InitializeComponent();
+        }
+
+        public Удали_меня_потом(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
